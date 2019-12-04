6834850be83c16d6b89d6daf0c68adcac97f22a2
Vasu Nayak <vasunayak@gmail.com>
2019-12-04 22:20:55 +1100

    - ADDED: spring boot starter project

---
+++ src/main/java/au/com/domainname/statusms/StatusmsApplication.java
@@ -0,0 +1,13 @@
+package au.com.domainname.statusms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/domainname/statusms/StatusmsApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.domainname.statusms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
